4c93688adea0fcd064a6b903c2cf138440deb4cf
null <raymuruvsanomuru@gmail.com>
2024-05-31 05:56:56 -0700

    add h1 in home

--- src/pages/platformContent/platformHome/platformHome.js
+++ src/pages/platformContent/platformHome/platformHome.js
@@ -92,6 +92,7 @@ const PlatformHome = () => {
 
     return (
         <motion.div className={cls.platformHome}>
+            <h1>isdoihhsads</h1>
             <h1>Hellodas dasd as dasd asd aadsasdasd asdasdasda</h1>
             <h1>adasdasdasdasd asd asd asd as</h1>
             <h1>
