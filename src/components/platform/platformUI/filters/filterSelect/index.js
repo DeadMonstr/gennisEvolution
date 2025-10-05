7e349569311d9dce0b2e4462b41ba8362afef98f
ikromovvv <isardor859@gmail.com>
2025-10-05 12:24:29 +0300

    fds

--- src/components/platform/platformUI/filters/filterSelect/index.js
+++ src/components/platform/platformUI/filters/filterSelect/index.js
@@ -2,36 +2,70 @@ import React, {useEffect, useState} from 'react';
 import Select from "components/platform/platformUI/select";
 import {useDispatch, useSelector} from "react-redux";
 import {setSelectOption} from "slices/filtersSlice";
+import {useHttp} from "hooks/http.hook";
+import {BackUrl, headers} from "constants/global";
 
-const FilterSelect = React.memo(({options,activeFilter,name,onChangeActiveFilter}) => {
-    const [option,setOption] = useState(null)
-    const {filters , activeFilters} = useSelector(state => state.filters)
-
+const FilterSelect = React.memo(({options, activeFilter, name, onChangeActiveFilter}) => {
+    const [option, setOption] = useState(null)
+    const {filters, activeFilters} = useSelector(state => state.filters)
 
 
     const dispatch = useDispatch()
     useEffect(() => {
         if (option) {
-            dispatch(setSelectOption({activeFilter : activeFilter, selectedOption: option}))
+            dispatch(setSelectOption({activeFilter: activeFilter, selectedOption: option}))
         }
-    },[option, activeFilter, dispatch])
+    }, [option, activeFilter, dispatch])
 
 
     const onChangeOption = (e) => {
         setOption(e)
         if (onChangeActiveFilter) {
-            onChangeActiveFilter(activeFilter,e)
+            onChangeActiveFilter(activeFilter, e)
         }
     }
 
-    console.log(activeFilters, "activeFilters[activeFilter]")
 
+    const [optionGroup, setOptionGroup] = useState(null)
+    const [optionDate, setOptionDate] = useState(null)
+
+    useEffect(() => {
+        if (optionGroup) {
+            dispatch(setSelectOption({activeFilter: "group", selectedOption: optionGroup}))
+        }
+    }, [optionGroup])
+    // useEffect(() => {
+    //     if (optionDate?.length){
+    //         setOptionGroup(optionDate[0]?.id)
+    //     }
+    // } , [optionDate])
+
+    const {request} = useHttp()
+    useEffect(() => {
+        if (activeFilters.teacher) {
+            request(`${BackUrl}group/groups_by_teacher/${activeFilters.teacher}`, "GET", null, headers())
+                .then(res => {
+                    setOptionDate(res.info)
+                })
+        }
+    }, [activeFilters?.teacher])
+
+    console.log(activeFilters , "active filter")
     return (
         <div>
 
-            <Select all={true} defaultValue={activeFilters[activeFilter]} onChangeOption={onChangeOption} options={options} name={"select-filter"} title={name}/>
+            <Select all={true} defaultValue={activeFilters[activeFilter]} onChangeOption={onChangeOption}
+                    options={options} name={"select-filter"} title={name}/>
+
+
+            { name === "O'qtuvchi" && <div style={{marginTop: "10px"}}>
+                <h2>Gruhlar:</h2>
+                <div>
+                    <Select all={true} defaultValue={optionGroup} onChangeOption={setOptionGroup} options={optionDate}/>
+                </div>
+            </div>}
         </div>
     );
-}) ;
+});
 
 export default FilterSelect;
