8ce18ee328c7c1cfd59ef404a2886c6e7c421ea2
null <raymuruvsanomuru@gmail.com>
2025-10-18 16:51:13 +0500

    fix

--- src/pages/platformContent/platformStudentsAttendance/platformStudentsAttendance.js
+++ src/pages/platformContent/platformStudentsAttendance/platformStudentsAttendance.js
@@ -1,5 +1,5 @@
-import React, {Fragment, useEffect, useState} from 'react';
-import {useDispatch, useSelector} from "react-redux";
+import React, { Fragment, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
 
 import Select from 'components/platform/platformUI/select';
 import Table from 'components/platform/platformUI/table';
@@ -7,13 +7,14 @@ import Input from 'components/platform/platformUI/input';
 import { fetchGroupsAttendance } from 'slices/groupsAttendanceSlice';
 
 import cls from "./platformStudentsAttendance.module.sass";
+import DefaultLoader from 'components/loader/defaultLoader/DefaultLoader';
 
 export const PlatformStudentsAttendance = () => {
 
     const dispatch = useDispatch()
 
-    const {attendance, attendanceCount, loading} = useSelector(state => state.groupsAttendanceSlice)
-    const {location} = useSelector(state => state.me)
+    const { attendance, attendanceCount, loading } = useSelector(state => state.groupsAttendanceSlice)
+    const { location } = useSelector(state => state.me)
     const [selectedDate, setSelectedDate] = useState(null)
 
     useEffect(() => {
@@ -23,14 +24,14 @@ export const PlatformStudentsAttendance = () => {
 
     useEffect(() => {
         // if (!selectedDate) {
-            // const savedDate = localStorage.getItem("selectedDate")
-            // if (savedDate) {
-                // setSelectedDate(savedDate)
-            // } else {
-                // const today = new Date().toISOString().split("T")[0]
-                // setSelectedDate(today)
-                // localStorage.setItem("selectedDate", today)
-            // }
+        // const savedDate = localStorage.getItem("selectedDate")
+        // if (savedDate) {
+        // setSelectedDate(savedDate)
+        // } else {
+        // const today = new Date().toISOString().split("T")[0]
+        // setSelectedDate(today)
+        // localStorage.setItem("selectedDate", today)
+        // }
         // }
 
         if (selectedDate && location) {
@@ -51,9 +52,9 @@ export const PlatformStudentsAttendance = () => {
                 <div className={cls.header}>
                     <h2 className={cls.header__title}>{item?.group_name}</h2>
                     <div className={cls.header__numbers}>
-                        <h2 className={cls.subTitle} style={{color: "#22C55E"}}>Kelganlar: {item?.summary?.present}</h2>
+                        <h2 className={cls.subTitle} style={{ color: "#22C55E" }}>Kelganlar: {item?.summary?.present}</h2>
                         <h2 className={cls.subTitle}
-                            style={{color: "#F43F5E"}}>Kelmaganlar: {item?.summary?.absent}</h2>
+                            style={{ color: "#F43F5E" }}>Kelmaganlar: {item?.summary?.absent}</h2>
                         <h2 className={cls.subTitle}>Umumiy: {item?.summary?.total}</h2>
                     </div>
                 </div>
@@ -63,22 +64,22 @@ export const PlatformStudentsAttendance = () => {
                         ? item?.students.map((itemIn, i) => (
 
                             <tbody>
-                            <tr>
-                                <td>{i + 1}</td>
-                                <td>{itemIn?.name} {itemIn?.surname}</td>
-                                <td>
-                                    {
-                                        itemIn?.status === null
-                                            ? "qilinmagan"
-                                            : itemIn?.status ? (
-                                                <i className="fas fa-check" style={{color: "#22C55E"}}/>
-                                            ) : (
-                                                <i className="fas fa-times" style={{color: "#F43F5E"}}/>
-                                            )
-                                    }
-                                </td>
-                                <td>{!itemIn?.status ? itemIn?.reason : "—"}</td>
-                            </tr>
+                                <tr>
+                                    <td>{i + 1}</td>
+                                    <td>{itemIn?.name} {itemIn?.surname}</td>
+                                    <td>
+                                        {
+                                            itemIn?.status === null
+                                                ? "qilinmagan"
+                                                : itemIn?.status ? (
+                                                    <i className="fas fa-check" style={{ color: "#22C55E" }} />
+                                                ) : (
+                                                    <i className="fas fa-times" style={{ color: "#F43F5E" }} />
+                                                )
+                                        }
+                                    </td>
+                                    <td>{!itemIn?.status ? itemIn?.reason : "—"}</td>
+                                </tr>
                             </tbody>
 
                         ))
@@ -89,6 +90,12 @@ export const PlatformStudentsAttendance = () => {
         ))
     }
 
+    console.log(loading);
+
+    // if (!!loading) {
+    //     return <DefaultLoader />
+    // }
+
     return (
         <div className={cls.studentAttendance}>
             <div className={cls.studentAttendance__header}>
@@ -96,9 +103,9 @@ export const PlatformStudentsAttendance = () => {
                     {/* <h1>Davomat</h1> */}
                     <div className={cls.subTitles}>
                         <h2 className={cls.subTitle}
-                            style={{color: "#22C55E"}}>Kelganlar: {attendanceCount?.present}</h2>
+                            style={{ color: "#22C55E" }}>Kelganlar: {attendanceCount?.present}</h2>
                         <h2 className={cls.subTitle}
-                            style={{color: "#F43F5E"}}>Kelmaganlar: {attendanceCount?.absent}</h2>
+                            style={{ color: "#F43F5E" }}>Kelmaganlar: {attendanceCount?.absent}</h2>
                         <h2 className={cls.subTitle}>Umumiy: {attendanceCount?.total}</h2>
                     </div>
                 </div>
@@ -121,21 +128,25 @@ export const PlatformStudentsAttendance = () => {
                         options={attendanceYears}
                         defaultValue={selectedYear}
                     /> */}
-                    <Input defaultValue={selectedDate} type={"date"} onChange={setSelectedDate}/>
+                    <Input defaultValue={selectedDate} type={"date"} onChange={setSelectedDate} />
                 </div>
             </div>
             <div className={cls.studentAttendance__container}>
-                <Table>
-                    <thead style={{top: 0, position: "initial"}}>
-                    <tr>
-                        <th>No</th>
-                        <th>Ism Familiya</th>
-                        <th>Davomat</th>
-                        <th>Sababi</th>
-                    </tr>
-                    </thead>
-                    {render()}
-                </Table>
+                {
+                    loading
+                        ? <DefaultLoader />
+                        : <Table>
+                            <thead style={{ top: 0, position: "initial" }}>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Ism Familiya</th>
+                                    <th>Davomat</th>
+                                    <th>Sababi</th>
+                                </tr>
+                            </thead>
+                            {render()}
+                        </Table>
+                }
             </div>
         </div>
     );
